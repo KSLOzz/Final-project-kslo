b0b30ac37a6d47ee4660f27bb2df645fab08df51
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:25:44 -0500

    Update Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,1 +1,19 @@
+#include "Client.h"
+#include <iostream>
+
+Client::Client(const std::string &n, const std::string &a,
+               const std::string &s, const std::string &e, double i)
+    : name(n), address(a), ssn(s), employer(e), income(i) {}
+
+const std::string& Client::getName() const { return name; }
+void Client::updateAddress(const std::string &a) { address = a; }
+void Client::updateEmployer(const std::string &e) { employer = e; }
+void Client::updateIncome(double i) { income = i; }
+
+void Client::display() const {
+    std::cout << "Address: " << address << "\n"
+              << "Social security number: " << ssn << "\n"
+              << "Employer: " << employer << "\n"
+              << "Annual income: " << income << "\n";
+}
 
