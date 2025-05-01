c109f6a67a9f677b750623c8ce3d028762533ea2
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:26:36 -0500

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,1 +1,28 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+#include "Client.h"
+
+enum class AccountType { CHECKING, SAVINGS };
+
+class Account {
+public:
+    Account() = default;
+    Account(int num, AccountType t, double b, Client* o);
+
+    int getNumber() const;
+    double getBalance() const;
+    bool deposit(double amt);
+    bool withdraw(double amt);
+    Client* getOwner() const;
+    AccountType getType() const;
+
+private:
+    int accountNumber;
+    AccountType accountType;
+    double balance;
+    Client* owner;
+};
+
+#endif // ACCOUNT_H
 
