68b5131574379e54c4f85d7565f56209d2126a10
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:25:06 -0500

    Update Client.h

--- Client.h
+++ Client.h
@@ -1,1 +1,24 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <string>
+
+class Client {
+public:
+    Client() = default;
+    Client(const std::string &n, const std::string &a,
+           const std::string &s, const std::string &e, double i);
+
+    const std::string& getName() const;
+    void updateAddress(const std::string &a);
+    void updateEmployer(const std::string &e);
+    void updateIncome(double i);
+    void display() const;
+
+private:
+    std::string name, address, ssn, employer;
+    double income;
+};
+
+#endif // CLIENT_H
 
