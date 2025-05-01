00c524cead809ebe0cdcbabff21cea6a7832db76
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:24:33 -0500

    Update AuthenticationManager.h

--- AuthenticationManager.h
+++ AuthenticationManager.h
@@ -1,1 +1,17 @@
+#ifndef AUTHENTICATIONMANAGER_H
+#define AUTHENTICATIONMANAGER_H
+
+#include "Teller.h"
+#include "UserManager.h"
+
+class AuthenticationManager {
+public:
+    AuthenticationManager(UserManager &um);
+    Teller* authenticate();
+
+private:
+    UserManager &userMgr;
+};
+
+#endif // AUTHENTICATIONMANAGER_H
 
