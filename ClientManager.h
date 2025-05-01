9b9e37692b7189b85cae3e2cd91cdbdbd5ad2610
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:26:00 -0500

    Update ClientManager.h

--- ClientManager.h
+++ ClientManager.h
@@ -1,1 +1,23 @@
+#ifndef CLIENTMANAGER_H
+#define CLIENTMANAGER_H
+
+#include "Client.h"
+#include <vector>
+#include <string>
+
+class ClientManager {
+public:
+    ClientManager();
+    bool addClient(const Client &c);
+    Client* getClient(const std::string &name);
+    bool editClient(const std::string &name);
+    void loadFromFile();
+    void saveToFile() const;
+
+private:
+    std::vector<Client> clients;
+    const std::string clientFile = "client-info";
+};
+
+#endif // CLIENTMANAGER_H
 
