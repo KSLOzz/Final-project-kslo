4b986825179e3db5614929acadceca7685d2adb3
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:26:53 -0500

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -1,1 +1,23 @@
+#include "Account.h"
+
+Account::Account(int num, AccountType t, double b, Client* o)
+    : accountNumber(num), accountType(t), balance(b), owner(o) {}
+
+int Account::getNumber() const { return accountNumber; }
+double Account::getBalance() const { return balance; }
+
+bool Account::deposit(double amt) {
+    if (amt <= 0) return false;
+    balance += amt;
+    return true;
+}
+
+bool Account::withdraw(double amt) {
+    if (amt <= 0 || amt > balance) return false;
+    balance -= amt;
+    return true;
+}
+
+Client* Account::getOwner() const { return owner; }
+AccountType Account::getType() const { return accountType; }
 
