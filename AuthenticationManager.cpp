b9eecff9fea206ff00808f1b5c04ff036ab6f75d
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:24:49 -0500

    Update AuthenticationManager.cpp

--- AuthenticationManager.cpp
+++ AuthenticationManager.cpp
@@ -1,1 +1,17 @@
+#include "AuthenticationManager.h"
+#include <iostream>
+
+AuthenticationManager::AuthenticationManager(UserManager &um)
+    : userMgr(um) {}
+
+Teller* AuthenticationManager::authenticate() {
+    std::string user, pwd;
+    std::cout << "User Name: "; std::getline(std::cin, user);
+    std::cout << "Password: ";  std::getline(std::cin, pwd);
+    for (auto &t : userMgr.listUsers()) {
+        if (t.getUsername() == user && t.verifyPassword(pwd))
+            return &t;
+    }
+    return nullptr;
+}
 
