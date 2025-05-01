dc2b30d755d595216066273e434aca9bac6f0199
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:23:49 -0500

    Update UserManager.h

--- UserManager.h
+++ UserManager.h
@@ -1,1 +1,23 @@
+#ifndef USERMANAGER_H
+#define USERMANAGER_H
+
+#include "Teller.h"
+#include <vector>
+#include <string>
+
+class UserManager {
+public:
+    UserManager();
+    bool addUser(const Teller &t);
+    bool removeUser(const std::string &username);
+    std::vector<Teller>& listUsers();
+    void loadFromFile();
+    void saveToFile() const;
+
+private:
+    std::vector<Teller> staffList;
+    const std::string staffFile = "staff";
+};
+
+#endif // USERMANAGER_H
 
