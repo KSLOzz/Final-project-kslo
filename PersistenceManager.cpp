49dcff4a1477a0bcf0f08d24e785f7f06bf10a2a
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:27:56 -0500

    Update PersistenceManager.cpp

--- PersistenceManager.cpp
+++ PersistenceManager.cpp
@@ -1,1 +1,19 @@
+#include "PersistenceManager.h"
+
+PersistenceManager::PersistenceManager(UserManager &um,
+                                       ClientManager &cm,
+                                       AccountManager &am)
+    : userMgr(um), clientMgr(cm), accountMgr(am) {}
+
+void PersistenceManager::loadAll() {
+    userMgr.loadFromFile();
+    clientMgr.loadFromFile();
+    accountMgr.loadFromFile();
+}
+
+void PersistenceManager::saveAll() {
+    userMgr.saveToFile();
+    clientMgr.saveToFile();
+    accountMgr.saveToFile();
+}
 
