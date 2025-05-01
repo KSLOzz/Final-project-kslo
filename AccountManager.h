95a40d2ca94578c1f7a2fdcefe24486342c2a22e
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:27:07 -0500

    Update AccountManager.h

--- AccountManager.h
+++ AccountManager.h
@@ -1,1 +1,23 @@
+#ifndef ACCOUNTMANAGER_H
+#define ACCOUNTMANAGER_H
+
+#include "Account.h"
+#include "ClientManager.h"
+#include <vector>
+
+class AccountManager {
+public:
+    AccountManager(ClientManager &cm);
+    bool addAccount(const Account &a);
+    Account* getAccount(int num);
+    void loadFromFile();
+    void saveToFile() const;
+
+private:
+    std::vector<Account> accounts;
+    ClientManager &clientMgr;
+    const std::string accountFile = "account-info";
+};
+
+#endif // ACCOUNTMANAGER_H
 
