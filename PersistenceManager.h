5215608a51a2ec00166ffe06113c7ca93854d042
KSLOzz <kds0090@auburn.edu>
2025-04-30 23:27:38 -0500

    Update PersistenceManager.h

--- PersistenceManager.h
+++ PersistenceManager.h
@@ -1,1 +1,23 @@
+#ifndef PERSISTENCEMANAGER_H
+#define PERSISTENCEMANAGER_H
+
+#include "UserManager.h"
+#include "ClientManager.h"
+#include "AccountManager.h"
+
+class PersistenceManager {
+public:
+    PersistenceManager(UserManager &um,
+                       ClientManager &cm,
+                       AccountManager &am);
+    void loadAll();
+    void saveAll();
+
+private:
+    UserManager &userMgr;
+    ClientManager &clientMgr;
+    AccountManager &accountMgr;
+};
+
+#endif // PERSISTENCEMANAGER_H
 
